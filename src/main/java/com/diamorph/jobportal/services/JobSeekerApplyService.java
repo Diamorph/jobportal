b08ef916112d5fe85a0d05a59b03c59db2dc1df5
diamorph <diamorph1309@gmail.com>
2025-02-24 21:29:21 -0500

    Apply for a job

---
+++ src/main/java/com/diamorph/jobportal/services/JobSeekerApplyService.java
@@ -0,0 +1,31 @@
+package com.diamorph.jobportal.services;
+
+import com.diamorph.jobportal.entity.JobPostActivity;
+import com.diamorph.jobportal.entity.JobSeekerApply;
+import com.diamorph.jobportal.entity.JobSeekerProfile;
+import com.diamorph.jobportal.repository.JobSeekerApplyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerApplyService {
+
+    private final JobSeekerApplyRepository jobSeekerApplyRepository;
+
+    public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
+        this.jobSeekerApplyRepository = jobSeekerApplyRepository;
+    }
+
+    public List<JobSeekerApply> getCandidatesJobs(JobSeekerProfile userAccountId) {
+        return jobSeekerApplyRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerApply> getJobCandidates(JobPostActivity job) {
+        return jobSeekerApplyRepository.findByJob(job);
+    }
+
+    public void addNew(JobSeekerApply jobSeekerApply) {
+        jobSeekerApplyRepository.save(jobSeekerApply);
+    }
+}
---
+++ src/main/java/com/diamorph/jobportal/services/JobSeekerSaveService.java
@@ -0,0 +1,26 @@
+package com.diamorph.jobportal.services;
+
+import com.diamorph.jobportal.entity.JobPostActivity;
+import com.diamorph.jobportal.entity.JobSeekerProfile;
+import com.diamorph.jobportal.entity.JobSeekerSave;
+import com.diamorph.jobportal.repository.JobSeekerSaveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerSaveService {
+    private final JobSeekerSaveRepository jobSeekerSaveRepository;
+
+    public JobSeekerSaveService(JobSeekerSaveRepository jobSeekerSaveRepository) {
+        this.jobSeekerSaveRepository = jobSeekerSaveRepository;
+    }
+
+    public List<JobSeekerSave> getCandidatesJob(JobSeekerProfile userAccountId) {
+        return jobSeekerSaveRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerSave> getJobCandidates(JobPostActivity job) {
+        return jobSeekerSaveRepository.findByJob(job);
+    }
+}
